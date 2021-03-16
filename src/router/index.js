8385158dba76b334f0777396dd20fb870417182a
katasu-git <katasu9923@gmail.com>
2021-03-16 13:56:20 +0900

    ログインページを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import CallTest from '../views/CallTest.vue'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'CallTest',
     component: CallTest
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
 ]
 
