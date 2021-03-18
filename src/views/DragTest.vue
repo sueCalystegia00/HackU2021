62ebfd7cd965cfe77e283fe7979ce2ca6dfc446b
katasu-git <katasu9923@gmail.com>
2021-03-18 14:36:31 +0900

    vue draggable導入

---
+++ src/views/DragTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Drag">
+    ドラッグ！
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Drag",
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+</style>
