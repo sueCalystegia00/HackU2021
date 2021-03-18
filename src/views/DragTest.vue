1f32f671bbd6bbd4bbf2c924469a514992531bda
katasu-git <katasu9923@gmail.com>
2021-03-18 14:39:42 +0900

    ドラッグ部分実装

--- src/views/DragTest.vue
+++ src/views/DragTest.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="Drag">
-    ドラッグ！
+    <draggable v-model="items" draggable=".item">
+      <div v-for="item in items" :key="item.id" class="item">
+        {{ item.name }}
+      </div>
+    </draggable>
+    <button @click="showItems">表示</button>
   </div>
 </template>
 
 <script>
+import vuedraggable from "vuedraggable";
+
 export default {
   name: "Drag",
+  components: {
+    draggable: vuedraggable
+  },
   data() {
     return {
+      items: [
+        {
+          id: 1,
+          name: "ITEM - 1",
+        },
+        {
+          id: 2,
+          name: "ITEM - 2",
+        },
+        {
+          id: 3,
+          name: "ITEM - 3",
+        },
+        {
+          id: 4,
+          name: "ITEM - 4",
+        },
+      ],
     };
   },
   methods: {
+    showItems() {
+      alert(
+        this.items.map((val) => {
+          return val.id;
+        })
+      );
+    },
   },
 };
 </script>
