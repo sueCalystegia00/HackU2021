5f47d52d6f7bdfb02acd59e9f560ef0ec397ce34
katasu-git <katasu9923@gmail.com>
2021-03-19 18:57:47 +0900

    メインにスタイルを追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<style>
+html,
+body,
+#app {
+  /*font-family: 'Noto Sans JP', sans-serif, 'Avenir', Helvetica, Arial, sans-serif;*/
+  height: 100%;
+  widows: 100%;
+  margin: 0;
+  padding: 0;
+}
+a,
+p,
+button {
+  margin: 0;
+  padding: 0;
+}
+</style>
