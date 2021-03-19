7e893c53396b42cbcb1cbcf1c139f4119d7203e2
katasu-git <katasu9923@gmail.com>
2021-03-19 19:33:02 +0900

    投入口のデザイン

--- src/components/InsertCoin.vue
+++ src/components/InsertCoin.vue
@@ -1,26 +1,42 @@
 <template>
   <div id="InsertCoin">
-      Coin
+    <div class="insertCircle">
+      <div class="insertBar"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'InsertCoin',
+  name: "InsertCoin",
   data() {
-    return {
-    };
+    return {};
   },
-}
+};
 </script>
 
 <style scoped>
-
-#InsertCoin{
-    width: 100%;
-    height: 100px;
-    background-color: chocolate;
+#InsertCoin {
+  width: 100%;
+  height: 100px;
+  background-color: chocolate;
 }
 
+.insertCircle {
+  width: 90px;
+  height: 90px;
+  border: solid 3px black;
+  border-radius: 100px;
+  background-color: #c4c4c4;
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.insertBar {
+  width: 6px;
+  height: 60px;
+  background-color: black;
+}
 </style>
